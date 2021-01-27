1fcc973bfcd4e89417a6395cc9e85a620be7a0ab
Pavol Virdzek <pavol.virdzek@avast.com>
2021-01-27 11:37:45 +0100

    + Added clicking on 'More languages…' button.

--- app/src/main/java/sk/palistudios/accessibilityclicker/MyAccesibilityService.kt
+++ app/src/main/java/sk/palistudios/accessibilityclicker/MyAccesibilityService.kt
@@ -3,6 +3,7 @@ package sk.palistudios.accessibilityclicker
 import android.accessibilityservice.AccessibilityService
 import android.util.Log
 import android.view.accessibility.AccessibilityEvent
+import android.view.accessibility.AccessibilityNodeInfo
 import android.widget.Toast
 
 class MyAccesibilityService() : AccessibilityService() {
@@ -11,11 +12,17 @@ class MyAccesibilityService() : AccessibilityService() {
         Log.d("accesibilityService", event.toString())
         when (event.eventType) {
             AccessibilityEvent.TYPE_GESTURE_DETECTION_START -> {
-                Log.d("accesibilityService",NodeInfo.wrap(getRootInActiveWindow())!!.toViewHeirarchy())
+                Log.d(
+                    "accesibilityService",
+                    NodeInfo.wrap(getRootInActiveWindow())!!.toViewHeirarchy()
+                )
                 run {
                     //If the event has a source, let's print it out separately.
                     if (event.source != null) {
-                        Log.d("accesibilityService", NodeInfo.wrap(event.source)!!.toViewHeirarchy())
+                        Log.d(
+                            "accesibilityService",
+                            NodeInfo.wrap(event.source)!!.toViewHeirarchy()
+                        )
                     }
                 }
             }
@@ -27,6 +34,24 @@ class MyAccesibilityService() : AccessibilityService() {
                 if (event.source != null) {
                     Log.d("accesibilityService", NodeInfo.wrap(event.source)!!.toViewHeirarchy())
                 }
+            }
+        }
+        tryClickOnFbButton(event)
+    }
+
+    fun tryClickOnFbButton(event: AccessibilityEvent) {
+        if (rootInActiveWindow != null && event.packageName == "com.facebook.katana") {
+            val fbRootLinearLayout = rootInActiveWindow.getChild(0)
+            for (i in 0 until fbRootLinearLayout.childCount) {
+                    val text = fbRootLinearLayout.getChild(i).text as String? ?: "null"
+                    Log.d(
+                        "accesibility, text:",
+                        (text)
+                    )
+                    if (text == "More languages…") {
+                        fbRootLinearLayout.getChild(i)
+                            .performAction(AccessibilityNodeInfo.ACTION_CLICK)
+                    }
             }
         }
     }
